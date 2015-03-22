94136323cb10d7d320a00ca356ef456e507ba5dd
John Gallagher <john@synapticmishap.co.uk>
2015-03-22 10:21:00 +0000

    Added failing tests for NSDates

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -23,4 +23,20 @@ describe "Application 'failing_cdq_test'" do
     Entry.create(startedAt: Time.new(2014, 6, 2, 0, 0, 1)).startedAt.should == Time.new(2014, 6, 2, 0, 0, 1)
     Entry.create(startedAt: Time.new(2014, 6, 2, 0, 0, 3)).startedAt.should == Time.new(2014, 6, 2, 0, 0, 3)
   end
+
+  it 'accurately sets startedAt for even seconds for NSDates' do
+    Entry.create(startedAt: gmt_formatter.dateFromString("01/06/2014 23:00:00 GMT")).startedAt.should == gmt_formatter.dateFromString("01/06/2014 23:00:00 GMT")
+    Entry.create(startedAt: gmt_formatter.dateFromString("01/06/2014 23:00:02 GMT")).startedAt.should == gmt_formatter.dateFromString("01/06/2014 23:00:02 GMT")
+  end
+
+  it 'accurately sets startedAt for odd seconds for NSDates' do
+    Entry.create(startedAt: gmt_formatter.dateFromString("01/06/2014 23:00:01 GMT")).startedAt.should == gmt_formatter.dateFromString("01/06/2014 23:00:01 GMT")
+    Entry.create(startedAt: gmt_formatter.dateFromString("01/06/2014 23:00:03 GMT")).startedAt.should == gmt_formatter.dateFromString("01/06/2014 23:00:03 GMT")
+  end
+end
+
+def gmt_formatter
+  formatter = NSDateFormatter.alloc.init
+  formatter.setDateFormat("dd/MM/yyyy HH:mm:ss z")
+  formatter
 end
